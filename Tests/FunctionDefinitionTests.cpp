ff4a432caf0b1b4f027533e339a5d7820a8a7746
tomfull123 <tom_full@hotmail.co.uk>
2024-02-23 20:52:19 +0000

    Updated tests and added function definition tests.

---
+++ Tests/FunctionDefinitionTests.cpp
@@ -0,0 +1,25 @@
+#include "TranspileTest.h"
+
+TEST_F(TranspileTest, PublicFunctionDefinition)
+{
+	std::string input = R"(
+		func doStuff() -> void:
+			pass
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\t\tvoid doStuff()\n\t\t{\n\t\t}\n\n\tprivate:\n\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t\t\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(TranspileTest, PrivateFunctionDefinition)
+{
+	std::string input = R"(
+		func _doStuff() -> void:
+			pass
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\tprivate:\n\n\t\tvoid _doStuff()\n\t\t{\n\t\t}\n\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t\t\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
