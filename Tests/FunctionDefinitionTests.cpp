d31961ccec78163208deec355035b368b3955ef1
tomfull123 <tom_full@hotmail.co.uk>
2024-02-24 15:01:21 +0000

    Added inferred function return type test.

--- Tests/FunctionDefinitionTests.cpp
+++ Tests/FunctionDefinitionTests.cpp
@@ -24,6 +24,18 @@ TEST_F(TranspileTest, PublicFunctionDefinitionWithArgs)
 	EXPECT_EQ(expected, actual);
 }
 
+TEST_F(TranspileTest, PublicFunctionDefinitionInferredReturnType)
+{
+	std::string input = R"(
+		func doStuff():
+			pass
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\t\tvoid doStuff()\n\t\t{\n\t\t}\n\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t\tClassDB::bind_method(D_METHOD(\"doStuff\"), &Test::doStuff);\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
+
 TEST_F(TranspileTest, PrivateFunctionDefinition)
 {
 	std::string input = R"(
