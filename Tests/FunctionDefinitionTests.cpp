c6a1328e6fbfb52d3912e8175ee71482df7ea1c0
tomfull123 <tom_full@hotmail.co.uk>
2024-02-24 13:29:28 +0000

    Added tests for function definitions with arguments.

--- Tests/FunctionDefinitionTests.cpp
+++ Tests/FunctionDefinitionTests.cpp
@@ -12,6 +12,18 @@ TEST_F(TranspileTest, PublicFunctionDefinition)
 	EXPECT_EQ(expected, actual);
 }
 
+TEST_F(TranspileTest, PublicFunctionDefinitionWithArgs)
+{
+	std::string input = R"(
+		func doStuff(x: String, y: int) -> void:
+			pass
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\t\tvoid doStuff(String x, int y)\n\t\t{\n\t\t}\n\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t\tClassDB::bind_method(D_METHOD(\"doStuff\", \"x\", \"y\"), &Test::doStuff);\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
+
 TEST_F(TranspileTest, PrivateFunctionDefinition)
 {
 	std::string input = R"(
@@ -21,5 +33,17 @@ TEST_F(TranspileTest, PrivateFunctionDefinition)
 
 	auto actual = transpile(input);
 	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\tprivate:\n\n\t\tvoid _doStuff()\n\t\t{\n\t\t}\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(TranspileTest, PrivateFunctionDefinitionWithArgs)
+{
+	std::string input = R"(
+		func _doStuff(x: String, y: int) -> void:
+			pass
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\tprivate:\n\n\t\tvoid _doStuff(String x, int y)\n\t\t{\n\t\t}\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n}\n";
 	EXPECT_EQ(expected, actual);
 }
