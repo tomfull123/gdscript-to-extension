c0bfbc0550c2fe9ceb76b85b03af0077dd179ea4
tomfull123 <tom_full@hotmail.co.uk>
2024-02-24 22:05:05 +0000

    Added test to infer argument types.

--- Tests/FunctionDefinitionTests.cpp
+++ Tests/FunctionDefinitionTests.cpp
@@ -59,3 +59,15 @@ TEST_F(TranspileTest, PrivateFunctionDefinitionWithArgs)
 	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\tprivate:\n\n\t\tvoid _doStuff(String x, int y)\n\t\t{\n\t\t}\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n}\n";
 	EXPECT_EQ(expected, actual);
 }
+
+TEST_F(TranspileTest, PrivateFunctionDefinitionArgsTypeInferred)
+{
+	std::string input = R"(
+		func _doStuff(x) -> void:
+			pass
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\tprivate:\n\n\t\tvoid _doStuff(Variant x)\n\t\t{\n\t\t}\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
