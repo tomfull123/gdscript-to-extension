9ce699b7a8fc03f09513d7dcfbe51447a2a6aec5
tomfull123 <tom_full@hotmail.co.uk>
2024-02-24 15:53:42 +0000

    Added tests for arrays.

---
+++ Tests/ArrayTests.cpp
@@ -0,0 +1,34 @@
+#include "TranspileTest.h"
+
+TEST_F(TranspileTest, EmptyArray)
+{
+	std::string input = R"(
+		var x: Array[int] = []
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\tprivate:\n\t\tArray x = [];\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(TranspileTest, ArrayType)
+{
+	std::string input = R"(
+		var x: Array[int]
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\tprivate:\n\t\tArray x;\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(TranspileTest, ArrayWithValues)
+{
+	std::string input = R"(
+		var x: Array[int] = [1, 2, 3]
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\tprivate:\n\t\tArray x = [\n\n\n];\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
