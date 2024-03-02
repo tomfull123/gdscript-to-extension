2bd2411ec0e4832eb8325617064147d63e7954ff
tomfull123 <tom_full@hotmail.co.uk>
2024-03-02 19:31:46 +0000

    Added a test for tool annotations.

--- Tests/ClassTests.cpp
+++ Tests/ClassTests.cpp
@@ -21,3 +21,14 @@ TEST_F(TranspileTest, ExtendsNode3D)
 	std::string expected = "#pragma once\n\n\nnamespace godot\n{\n\tclass Test : public Node3D\n\t{\n\t\tGDCLASS(Test, Node3D)\n\tpublic:\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n}\n";
 	EXPECT_EQ(expected, actual);
 }
+
+TEST_F(TranspileTest, ToolAnnotation)
+{
+	std::string input = R"(
+		@tool
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
