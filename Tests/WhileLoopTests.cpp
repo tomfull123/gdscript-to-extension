304da615c05ad2d84e6022c96efac19d165df1f9
tomfull123 <tom_full@hotmail.co.uk>
2024-04-16 20:52:32 +0100

    Added tests for the break and continue keywords.

--- Tests/WhileLoopTests.cpp
+++ Tests/WhileLoopTests.cpp
@@ -13,3 +13,33 @@ TEST_F(TranspileTest, WhileLoop)
 	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\t\tvoid doStuff()\n\t\t{\n\t\t\tint x = 0;\n\t\t\twhile ((x < 10))\n\t\t\t{\n\t\t\t\tx = (x + 1);\n\t\t\t}\n\t\t}\n\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t\tClassDB::bind_method(D_METHOD(\"doStuff\"), &Test::doStuff);\n\t\t}\n\t};\n}\n";
 	EXPECT_EQ(expected, actual);
 }
+
+TEST_F(TranspileTest, WhileBreak)
+{
+	std::string input = R"(
+		func doStuff() -> void:
+			var x := 0
+			while x < 10:
+				x = x + 1
+				break
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\t\tvoid doStuff()\n\t\t{\n\t\t\tint x = 0;\n\t\t\twhile ((x < 10))\n\t\t\t{\n\t\t\t\tx = (x + 1);\n\t\t\t\tbreak;\n\t\t\t}\n\t\t}\n\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t\tClassDB::bind_method(D_METHOD(\"doStuff\"), &Test::doStuff);\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(TranspileTest, WhileContinue)
+{
+	std::string input = R"(
+		func doStuff() -> void:
+			var x := 0
+			while x < 10:
+				x = x + 1
+				continue
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\t\tvoid doStuff()\n\t\t{\n\t\t\tint x = 0;\n\t\t\twhile ((x < 10))\n\t\t\t{\n\t\t\t\tx = (x + 1);\n\t\t\t\tcontinue;\n\t\t\t}\n\t\t}\n\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t\tClassDB::bind_method(D_METHOD(\"doStuff\"), &Test::doStuff);\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
