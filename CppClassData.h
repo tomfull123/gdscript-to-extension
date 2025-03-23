f15ba09a17117ca68e2658cbf2fa36859c41d109
Tom <tom_full@hotmail.co.uk>
2025-03-23 01:54:29 +0000

    Extracted out the CppClassData struct into header and source files.

---
+++ CppClassData.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include "GDLexer.h"
+#include "Type.h"
+
+class VariableDefinitionSyntaxNode;
+class FunctionPrototypeSyntaxNode;
+class EnumDefinitionSyntaxNode;
+class ValueSyntaxNode;
+struct CppData;
+
+struct CppClassData
+{
+	std::unordered_set<std::string> types;
+	std::unordered_set<std::string> externalFunctions;
+	std::unordered_map<std::string, VariableDefinitionSyntaxNode*> variableDefinitions;
+	std::unordered_map<std::string, FunctionPrototypeSyntaxNode*> functionPrototypeDefinitions;
+	std::unordered_map<std::string, EnumDefinitionSyntaxNode*> enumDefinitions;
+	std::unordered_set<std::string> typeDefinitions;
+	std::string currentClassName;
+	Type* classInheritedType;
+
+	std::string toCppType(const Type* type);
+
+	std::string toCppFunction(const std::string& functionName, const std::string& parentType);
+
+	std::string toWrappedCppFunction(ValueSyntaxNode* parentInstance, const GDToken* nameToken) const;
+
+	std::string getIncludePath(const std::string& type) const;
+
+	bool isGodotType(const std::string& type) const;
+
+	bool isClassMethod(const std::string& name, const CppData* data) const;
+
+	static bool isGodotTypeMethod(const Type* parentType, const std::string& name, const CppData* data);
+
+	static bool isProperty(ValueSyntaxNode* parentInstance, const GDToken* name, const CppData* data);
+};
