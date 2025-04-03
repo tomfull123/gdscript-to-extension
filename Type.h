332fe3c2906aa3c84084f5e76a5f83cfcb33bd20
Tom <tom_full@hotmail.co.uk>
2025-04-04 00:52:10 +0100

    Added toString method to the Type class.

--- Type.h
+++ Type.h
@@ -22,4 +22,20 @@ struct Type
 
 	std::string name;
 	std::vector<Type*> subtypes;
+
+	std::string toString()
+	{
+		std::string subtypeString;
+
+		int lastIndex = subtypes.size() - 1;
+
+		for (int i = 0; i < subtypes.size(); i++)
+		{
+			auto subtype = subtypes[i];
+			subtypeString += subtype->toString();
+			if (i != lastIndex) subtypeString += ", ";
+		}
+
+		return name + "<" + subtypeString + ">";
+	}
 };
