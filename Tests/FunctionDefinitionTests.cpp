6a11ec7638f701bbb3826eefa54e3a4105bfba82
Tom <tom_full@hotmail.co.uk>
2025-04-18 00:53:57 +0100

    Added a test for namespaced types

--- Tests/FunctionDefinitionTests.cpp
+++ Tests/FunctionDefinitionTests.cpp
@@ -232,3 +232,20 @@ TEST_F(TranspileTest, PublicFunctionDefinitionWithUnderscoredClassTypeArg)
 	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tclass MyClass : public RefCounted\n\t{\n\t\tGDCLASS(MyClass, RefCounted)\n\tpublic:\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\t\tvoid doStuff(Ref<MyClass> x)\n\t\t{\n\t\t}\n\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t\tClassDB::bind_method(D_METHOD(\"doStuff\", \"x\"), &Test::doStuff);\n\t\t}\n\t};\n}\n";
 	EXPECT_EQ(expected, actual);
 }
+
+TEST_F(TranspileTest, FunctionDefinitionNamespaceTypeArg)
+{
+	std::string input = R"(
+		class MyClass:
+			enum Type {
+				Type1
+			}
+
+		func doStuff(x: MyClass.Type) -> void:
+			pass
+	)";
+
+	auto actual = transpile(input);
+	std::string expected = "#pragma once\n\n#include <godot_cpp/classes/ref.hpp>\n\nnamespace godot\n{\n\tenum Type\n\t{\n\t\tType1,\n\t};\n\tclass MyClass : public RefCounted\n\t{\n\t\tGDCLASS(MyClass, RefCounted)\n\tpublic:\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t}\n\t};\n\n\tclass Test : public RefCounted\n\t{\n\t\tGDCLASS(Test, RefCounted)\n\tpublic:\n\t\tvoid doStuff(Type x)\n\t\t{\n\t\t}\n\n\tprivate:\n\n\tprotected:\n\t\tstatic void _bind_methods()\n\t\t{\n\t\t\tClassDB::bind_method(D_METHOD(\"doStuff\", \"x\"), &Test::doStuff);\n\t\t}\n\t};\n}\n";
+	EXPECT_EQ(expected, actual);
+}
