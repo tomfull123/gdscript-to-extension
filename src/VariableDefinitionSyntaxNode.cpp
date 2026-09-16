a9db028d94e30eaf864b783f2db8cf1d98333ec5
Tom <tom_full@hotmail.co.uk>
2026-09-16 01:23:26 +0100

    Moved the hoist, resolveDefinitions and resolveTypes method definitions into a cpp file for the VariableDefinitionSyntaxNode class

---
+++ src/VariableDefinitionSyntaxNode.cpp
@@ -0,0 +1,33 @@
+#include "VariableDefinitionSyntaxNode.h"
+
+void VariableDefinitionSyntaxNode::hoist(CppData* data)
+{
+	if (isClassMember_ || isStatic_) data->currentClass->memberVariableDefinitions[name_->value] = this;
+	else data->currentClass->currentFunction->variableDefinitions[name_->value] = this;
+	if (initialValue_) initialValue_->hoist(data);
+}
+
+void VariableDefinitionSyntaxNode::resolveDefinitions(CppData* data)
+{
+	if (initialValue_) initialValue_->resolveDefinitions(data);
+}
+
+void VariableDefinitionSyntaxNode::resolveTypes(CppData* data, Type* otherType = nullptr)
+{
+	if (initialValue_)
+	{
+		initialValue_->resolveTypes(data);
+
+		if (!dataType_) dataType_ = initialValue_->getType();
+		else
+		{
+			auto initialValueType = initialValue_->getType();
+			if (initialValueType && dataType_->subtypes.size() < initialValueType->subtypes.size())
+			{
+				dataType_ = initialValueType;
+			}
+		}
+	}
+
+	if (!dataType_) dataType_ = otherType;
+}
